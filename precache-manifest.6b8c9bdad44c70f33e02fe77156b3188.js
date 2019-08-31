30b8da1de2eb3605fa90ca51663eb2c351f4d43b
Francisco do Vale <juniordev2@gmail.com>
2019-08-31 09:51:51 +0200

    Updates

---
+++ precache-manifest.6b8c9bdad44c70f33e02fe77156b3188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b0e82aaf151b028d19a55e8ff82f62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34514534f1c2984e7f6b",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d8d8e5e77ec070b85c3e",
+    "url": "/static/js/2.e4395c4b.chunk.js"
+  },
+  {
+    "revision": "34514534f1c2984e7f6b",
+    "url": "/static/js/main.0e1eab55.chunk.js"
+  },
+  {
+    "revision": "773acbd82050ab1cab8a",
+    "url": "/static/js/runtime~main.1740744e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
